7d349b1751934cd5e61c217123c2340d415ecba4
wqiang.wang <wqiang.wang@qunar.com>
2017-12-25 21:34:11 +0800

    init1

---
+++ src/main/java/cn/edu/scu/eie/Main.java
@@ -0,0 +1,16 @@
+package cn.edu.scu.eie;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: wqiang.wang
+ * Date: 2017/12/25
+ * Time: 21:18
+ */
+
+public class Main {
+    public void say() {
+        System.out.println("hello word!");
+    }
+}
---
+++ src/test/java/cn/edu/scu/eie/MainTest.java
@@ -0,0 +1,18 @@
+package cn.edu.scu.eie;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: wqiang.wang
+ * Date: 2017/12/25
+ * Time: 21:24
+ */
+
+public class MainTest {
+    Main main = new Main();
+    @Test
+    public void mainTest() {
+        main.say();
+    }
+}
